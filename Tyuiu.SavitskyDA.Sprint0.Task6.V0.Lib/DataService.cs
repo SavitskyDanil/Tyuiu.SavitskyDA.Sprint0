2afdf4f6d8d46ebc3aae6462a157b3340b0b4861
Tyuiu.SavitskyDA@outlook.com <Tyuiu.SavitskyDA@outlook.com>
2023-09-17 20:44:34 +0500

    Task6

---
+++ Tyuiu.SavitskyDA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SavitskyDA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            // Цикл параметрами for
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+        //Цикл с предусловием while
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+        }
+        //Цикл с постусловием do while
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.SavitskyDA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SavitskyDA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SavitskyDA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91bb6915-cb64-47f6-bb92-ad4473edf58c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
